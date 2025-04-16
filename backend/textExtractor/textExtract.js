160c6715bdcfa95e417f5cc85dbcdeca1fe6de3b
indadul-hoque <indadul.hoque9735@gmail.com>
2025-04-17 02:51:51 +0530

    Resolved merge conflicts between indadul and javed

---
+++ backend/textExtractor/textExtract.js
@@ -0,0 +1,37 @@
+import tesseract from 'tesseract.js';
+
+tesseract.recognize(
+    "../uploads/list.png",
+    "eng",
+    {
+        langPath: "./testdata", 
+    }
+)
+.then((
+    { data: { text } }) => {
+
+    // Every item with quantity is extracted in items without space, lines and empty lines
+    const items = text.split("\n").map(i => i.trim()).filter(Boolean)
+
+    // How many items in the list
+    const total_items = items.length
+    let itemArr = [];
+
+    items.forEach( (item, index) => {
+        const quan = item.match(/(\d+(\.\d+)?\s?[a-zA-Z]*)$/);       
+        
+                                                            // \d+       → Matches numbers  
+                                                            // (\.\d+)?  → Optionally matches decimal numbers  
+                                                            // \s?       → Optionally matches a space after the number  
+                                                            // [a-zA-Z]* → Matches optional units (letters only)  
+                                                            // $         → Ensures it matches only at the end of the string  
+        const itemQuant = quan ? quan[0] : "Null"           // quan[0] means full match, quan[1] only numeric part  
+        const itemName = quan ? item.replace(quan[0], "").trim() : item;
+        itemArr.push({itemName, itemQuant})
+        global[`item${index+1}Name`] = itemName;
+        global[`item${index+1}Quantity`] = itemQuant;
+    });
+    console.log(itemArr)
+    console.log("Total number of items : ",total_items)
+
+})
