4716bba5c5f65a3b4d39ac8d23fe9b6afd85a84d
indadul-hoque <indadul.hoque9735@gmail.com>
2025-06-05 00:09:52 +0530

    change in backend and frontend for authorization

--- backend/routers/cart.route.js
+++ backend/routers/cart.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { getUserCart, removeItemFromCart } from "../controlers/cart.controler.js";
 import { verifyToken } from "../middleware/verifyToken.js";
+import { authenticate } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
 // Get cart items for the logged-in user
-router.get("/user-cart", verifyToken, getUserCart);
-router.delete("/user-cart/:productId", verifyToken, removeItemFromCart);
+router.get("/user-cart", authenticate, getUserCart);
+router.delete("/user-cart/:productId", authenticate, removeItemFromCart);
 
 export default router;
--- backend/routers/products.route.js
+++ backend/routers/products.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { upload, createProduct, searchProductsByKeyword, getAllProducts, updateProduct, deleteProduct, getSingleProduct } from '../controlers/products.controler.js';
 import { verifyToken, roleBasedAccess } from '../middleware/verifyToken.js';
+import { authenticate } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 // Route to create a new product
-router.post('/createProduct', verifyToken, roleBasedAccess('admin'),  upload.array('images'),  createProduct);
+router.post('/createProduct', authenticate, roleBasedAccess('admin'),  upload.array('images'),  createProduct);
   
 
 // Route to get products by keyword
@@ -15,13 +16,13 @@ router.get('/searchProductsByKeyword', searchProductsByKeyword);
 router.get('/getAllProducts', getAllProducts);
 
 // Route to get a single product by ID
-router.get('/singleProduct/:id', verifyToken, getSingleProduct);
+router.get('/singleProduct/:id', authenticate, getSingleProduct);
 
 // Route to update a product by ID
-router.put('/updateProduct/:id', verifyToken, upload.array('images'), updateProduct);
+router.put('/updateProduct/:id', authenticate, upload.array('images'), updateProduct);
 
 
 // Route to delete a product by ID
-router.delete('/deleteProduct/:id', verifyToken, deleteProduct);
+router.delete('/deleteProduct/:id', authenticate, deleteProduct);
 
 export default router;
